7098bbf0337338ee148f0598bbf2f6af4cbdaf8b
kotobukid <kotobuki@daqua.jp>
2021-08-30 23:41:12 +0900

    Vue.jsのSingleFileComponentを表示

---
+++ ts/components/MainApp.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.main main app component
+
+</template>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import {Prop, Vue} from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+class MainApp extends Vue {
+  // @Prop({default: ''})
+  // readonly message!: string;
+}
+
+export default MainApp;
+
+</script>
+
+<style scoped lang="less"></style>
--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,12 @@
-window.onload = () => {
+import Vue from 'vue';
+import MainApp from './components/MainApp.vue';
 
+window.onload = () => {
+    new Vue({
+        el: '#main',
+        components: {
+            'main-app': MainApp
+        }
+    });
 }
 
